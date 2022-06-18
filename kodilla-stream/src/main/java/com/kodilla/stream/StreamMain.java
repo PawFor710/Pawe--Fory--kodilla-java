b612620d27d6a1d99f9369ceff8e5db02d92dd84
Pawel Forys <pawfor710@gmail.com>
2022-06-18 12:15:30 +0200

    7.1 Text Decorator

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -4,6 +4,8 @@ import com.kodilla.stream.beautifier.PoemBeautifier;
 import com.kodilla.stream.lambda.*;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
+import java.util.Locale;
+
 public class StreamMain {
     public static void main(String[] args) {
 
@@ -13,5 +15,13 @@ public class StreamMain {
                 textToDecorate -> textToDecorate + ".");
         poemBeautifier.beautify("This one needs exclamation sign",
                 textToDecorate -> textToDecorate + "!");
+        poemBeautifier.beautify("Do i need more sentence",
+                textDecorator -> textDecorator + "?");
+        poemBeautifier.beautify("let's make this sentence bigger",
+                textDecorator -> textDecorator.toUpperCase(Locale.ROOT));
+        poemBeautifier.beautify("BUT THIS SENTENCE IS GOING TO BE LOWER",
+                textDecorator -> textDecorator.toLowerCase());
+        poemBeautifier.beautify("How long is the last sentence?",
+                textDecorator -> textDecorator + " The answer is: " + textDecorator.length());
     }
 }
