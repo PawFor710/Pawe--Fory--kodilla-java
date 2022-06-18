ffddd401d97bf10461117e1b31071b21c2968eff
Pawel Forys <pawfor710@gmail.com>
2022-06-18 13:06:18 +0200

    7.2 immutable forumUser

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.*;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
@@ -8,7 +9,7 @@ import java.util.Locale;
 
 public class StreamMain {
     public static void main(String[] args) {
-
+        // 7.1
         PoemBeautifier poemBeautifier = new PoemBeautifier();
 
         poemBeautifier.beautify("This sentence needs a dot at the end",
@@ -23,5 +24,9 @@ public class StreamMain {
                 textDecorator -> textDecorator.toLowerCase());
         poemBeautifier.beautify("How long is the last sentence?",
                 textDecorator -> textDecorator + " The answer is: " + textDecorator.length());
+
+        //7.2
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
