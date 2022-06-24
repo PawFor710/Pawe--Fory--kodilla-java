794031dec4c4670a7ccf49c896158df4ffb4dd5d
Pawel Forys <pawfor710@gmail.com>
2022-06-24 21:53:30 +0200

    7.5 Obliczanie sredniej funkcyjnej

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -7,11 +7,15 @@ public interface ArrayOperations {
     public static double getAverage(int[] numbers) {
 
         IntStream.range(0, numbers.length)
+                .map(n -> numbers[n])
                 .forEach(System.out::println);
 
+
         double mean = IntStream.range(0, numbers.length)
+                .map(n -> numbers[n])
                 .average()
                 .getAsDouble();
+
         return mean;
     }
 }
--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -11,8 +11,8 @@ public class ArrayOperationsTestSuite {
 
         //Given
         //When
-        int[] tab = {2, 5, 8, 12, 22, 4, 12, 4, 24, 54, 33, 87, 56, 29, 16, 43, 27, 85, 97, 12};
-        double mean = ArrayOperations.getAverage(tab);
+        int[] numbers = {2, 5, 8, 12, 22, 4, 12, 4, 24, 54, 33, 87, 56, 29, 16, 43, 27, 85, 97, 12};
+        double mean = ArrayOperations.getAverage(numbers);
 
         //Then
         assertEquals(31.6, mean, 0.1);
