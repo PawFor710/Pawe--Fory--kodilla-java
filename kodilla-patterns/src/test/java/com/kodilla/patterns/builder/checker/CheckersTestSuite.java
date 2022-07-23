7f92aebe4a4a29a43a904746422a0bed43a42859
Pawel Forys <pawfor710@gmail.com>
2022-07-23 16:19:56 +0200

    12.6 aktywni uzytkownicy.

--- kodilla-patterns/src/test/java/com/kodilla/patterns/builder/checker/CheckersTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/builder/checker/CheckersTestSuite.java
@@ -14,16 +14,16 @@ public class CheckersTestSuite {
         Checkers checkers = new Checkers.CheckersBuilder()
                 .PlayerOne("John")
                 .PlayerTwo("Theresa")
-                .Figure(FigureFactory.PAWN, Figure.WHITE, 3, 0)
-                .Figure(FigureFactory.PAWN, Figure.BLACK, 7, 9)
-                .Figure(FigureFactory.QUEEN, Figure.WHITE, 2, 0)
+                .Figure(FigureFactory.PAWN, Figure.WHITE, 0, 3)
+                .Figure(FigureFactory.PAWN, Figure.BLACK, 9, 7)
+                .Figure(FigureFactory.QUEEN, Figure.WHITE, 0, 2)
                 .build();
         System.out.println(checkers.getBoard());
 
         //When
-        Figure figureOne = checkers.getBoard().getFigure(3, 0);
-        Figure figureTwo = checkers.getBoard().getFigure(7, 9);
-        Figure figureThree = checkers.getBoard().getFigure(2, 0);
+        Figure figureOne = checkers.getBoard().getFigure(0, 3);
+        Figure figureTwo = checkers.getBoard().getFigure(9, 7);
+        Figure figureThree = checkers.getBoard().getFigure(0, 2);
         Figure figureFour = checkers.getBoard().getFigure(1, 1);
 
         //Then
